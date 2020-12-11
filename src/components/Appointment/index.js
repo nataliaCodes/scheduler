390dfd50c50036d41d0b9353ecdc77d831c06206
Natalia Martian <natalia.martian@gmail.com>
2020-12-10 22:12:51 -0500

    Set up useVisualMode in Appointment index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,17 +2,33 @@ import React from 'react';
 import Header from "./Header"
 import Show from "./Show"
 import Empty from "./Empty"
-
+import useVisualMode from "../../hooks/useVisualMode"
 
 import "./styles.scss";
 
+//variables needed for the visual mode hook
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
 
+  //set the mode conditionally based on props.interview value
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
-  <article className="appointment">
-    <Header time={props.time} />
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={props.onEdit} onDelete={props.onDelete} /> : <Empty />}
-  </article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {/* show the components conditionally based on the visual mode hook */}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+    </article>
   )
 
 }
