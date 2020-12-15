16c1908fdceb27e8f383c5b9cd83a0944cbdd371
Natalia Martian <natalia.martian@gmail.com>
2020-12-14 21:17:14 -0500

    Change test suites to work as expected

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -3,11 +3,29 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 
 import Appointment from "components/Appointment";
-import { interviewers} from "./fixtures"
 
 afterEach(cleanup);
 
 describe("Appointment", () => {
+
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    },
+    {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png"
+    }
+  ]
+
   it("renders without crashing", () => {
     render(<Appointment interviewers={interviewers} />);
   });
